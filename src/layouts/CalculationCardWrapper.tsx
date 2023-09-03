572ba16df5854bca28b97a7cd0c71f18bae56d45
Patryk Keska <PatrykKeska4@gmail.com>
2023-09-03 22:06:20 +0200

    Changed width of card component

--- src/layouts/CalculationCardWrapper.tsx
+++ src/layouts/CalculationCardWrapper.tsx
@@ -17,12 +17,12 @@ export const CalculationCardWrapper = ({
       {!noDelete ? (
         <Link
           to={`/my-results/${id}`}
-          className=' relative block overflow-hidden rounded-lg border border-gray-100 p-4 pb-6 shadow-xl transition-all ease-in-out hover:rotate-2 hover:scale-105  hover:cursor-pointer dark:bg-slate-900 sm:p-6 lg:p-8'
+          className=' relative block w-80 overflow-hidden rounded-lg border border-gray-100 p-4 pb-6 shadow-xl transition-all ease-in-out hover:rotate-2 hover:scale-105  hover:cursor-pointer dark:bg-slate-900 sm:p-6 lg:p-8'
         >
           {children}
         </Link>
       ) : (
-        <div className=' relative block overflow-hidden rounded-lg border border-gray-100 p-4 pb-6 shadow-xl transition-all ease-in-out hover:rotate-2 hover:scale-105   dark:bg-slate-900 sm:p-6 lg:p-8'>
+        <div className=' relative block w-80 overflow-hidden rounded-lg border border-gray-100 p-4 pb-6 shadow-xl transition-all ease-in-out hover:rotate-2 hover:scale-105   dark:bg-slate-900 sm:p-6 lg:p-8'>
           {children}
         </div>
       )}
